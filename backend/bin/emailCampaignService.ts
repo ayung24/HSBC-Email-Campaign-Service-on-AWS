00c94b794f189c2979b0419e43613ddb1e9a4fb8
philmak <31200843+philmak@users.noreply.github.com>
2021-02-13 20:25:58 -0800

    Add cdk backend (#2)
    * Make new cdk project
    
    * Rename folder

---
+++ backend/bin/emailCampaignService.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from '@aws-cdk/core';
+import { EmailCampaignServiceStack } from '../lib/emailCampaignServiceStack';
+
+const app = new cdk.App();
+new EmailCampaignServiceStack(app, 'EmailCampaignServiceStack');
