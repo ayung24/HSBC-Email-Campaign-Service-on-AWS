4f6a8d2837c0619ad1ea4aa00bd889c85bd172e5
Tony Liu <phyberosis@gmail.com>
2021-02-15 15:52:05 -0800

    Interface + example for using db

---
+++ backend/lib/Database/dbOperations.ts
@@ -0,0 +1,6 @@
+import * as cdk from '@aws-cdk/core';
+import * as db from "./interfaces";
+
+export function AddMetadataEntry(data: db.IMetadataRecord): db.IDBResponse {
+    return {Succeeded: false, Info: `Not implemented but you gave ${JSON.stringify(data)}`}
+}
