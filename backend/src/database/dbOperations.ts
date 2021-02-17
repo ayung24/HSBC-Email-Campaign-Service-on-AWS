3c0f0a99ccd8cce6623d039b2075abac29f87b6e
Tony Liu <phyberosis@gmail.com>
2021-02-17 15:14:25 -0800

    update example

--- backend/src/database/dbOperations.ts
+++ backend/src/database/dbOperations.ts
@@ -1,28 +1,28 @@
 import { promises } from 'dns';
 import * as db from './interfaces';
 
-export function AddMetadataEntry(name: string): Promise<{response: db.IDBResponse, metadata?: db.IMetadataEntry}> {
-    return Promise.resolve({response: { succeeded: false, info: `Not implemented`}});
+export function AddMetadataEntry(name: string): Promise<{status: db.IDBResponse, metadata?: db.IMetadataEntry}> {
+    return Promise.resolve({status: { succeeded: false, info: `Not implemented`}});
     // metadata: {templateID: name: "name", timeCreated: new Date(), status: db.EntryStatus.IN_SERVICE}});
 }
 
-export function GetMetadataByID(id: string): Promise<{response: db.IDBResponse, metadata?: db.IMetadataEntry}> {
-    return Promise.resolve({response: { succeeded: false, info: `Not implemented`}});
+export function GetMetadataByID(id: string): Promise<{status: db.IDBResponse, metadata?: db.IMetadataEntry}> {
+    return Promise.resolve({status: { succeeded: false, info: `Not implemented`}});
 }
 
-export function ListMetadataByDate(start: Date, end:Date): Promise<{response: db.IDBResponse, metadataList?: db.IMetadataEntry[]}> {
-    return Promise.resolve({response: { succeeded: false, info: `Not implemented`}});
+export function ListMetadataByDate(start: Date, end: Date): Promise<{status: db.IDBResponse, metadataList?: db.IMetadataEntry[]}> {
+    return Promise.resolve({status: { succeeded: false, info: `Not implemented`}});
 }
 
-export function GetMetadataByName(name: string): Promise<{response: db.IDBResponse, metadata?: db.IMetadataEntry}> {
-    return Promise.resolve({response: { succeeded: false, info: `Not implemented`}});
+export function GetMetadataByName(name: string): Promise<{status: db.IDBResponse, metadata?: db.IMetadataEntry}> {
+    return Promise.resolve({status: { succeeded: false, info: `Not implemented`}});
 }
 
 // add metadata to get templateID
-export function AddHTMLEntry(htmlEntry: db.IHTMLEntry): Promise<{response: db.IDBResponse}> {
-    return Promise.resolve({response: { succeeded: false, info: `Not implemented`}});
+export function AddHTMLEntry(htmlEntry: db.IHTMLEntry): Promise<{status: db.IDBResponse}> {
+    return Promise.resolve({status: { succeeded: false, info: `Not implemented`}});
 }
 
-export function GetHTMLByID(id: string): Promise<{response: db.IDBResponse, html?: db.IHTMLEntry}> {
-    return Promise.resolve({response: { succeeded: false, info: `Not implemented`}});
+export function GetHTMLByID(id: string): Promise<{status: db.IDBResponse, html?: db.IHTMLEntry}> {
+    return Promise.resolve({status: { succeeded: false, info: `Not implemented`}});
 }
