8e7c8ed8fc93713cee5ddcc28b90646c5bd42591
tianyan-zhu <tianyan.j.zhu@gmail.com>
2021-02-17 15:16:55 -0800

    Generates an ecrpyted API Key

--- backend/src/lambda/uploadTemplate/index.ts
+++ backend/src/lambda/uploadTemplate/index.ts
@@ -3,6 +3,56 @@ import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult, Ha
 import { v4 as uuid } from 'uuid';
 import { IUploadTemplateRequestBody } from '../types';
 
+async function mySecrets(): Promise<string> {
+    // Load the AWS SDK
+    var AWS = require('aws-sdk'),
+        region = "ca-central-1",
+        secretName = "API_Key_Secret",
+        secret,
+        decodedBinarySecret;
+
+    // Create a Secrets Manager client
+    var client = new AWS.SecretsManager({
+        region: region
+    });
+
+    return new Promise((resolve,reject)=>{
+        client.getSecretValue({SecretId: secretName}, function(err: any, data: any) {
+
+            // In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+            // See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+            // We rethrow the exception by default.
+            if (err) {
+                reject(err);
+            }
+            else {
+                // Decrypts secret using the associated KMS CMK.
+                // Depending on whether the secret is a string or binary, one of these fields will be populated.
+                if ('SecretString' in data) {
+                    resolve(data.SecretString);
+                } else {
+                    let buff = new Buffer(data.SecretBinary, 'base64');
+                    resolve(buff.toString('ascii'));
+                }
+            }
+        });
+    });
+}
+
+async function generateEncryptedApiKey(): Promise<string> {
+    const uuidAPIKey = require('uuid-apikey');
+    const Cryptr = require('cryptr');
+
+    const apiKeyUuid: string = uuid();
+    const apiKey: string = uuidAPIKey.toAPIKey(apiKeyUuid);
+
+    const secretValue: string = await mySecrets();
+    const cryptr = new Cryptr(secretValue);
+    const encryptedApiKey:string = cryptr.encrypt(apiKey);
+
+    return encryptedApiKey;
+}
+
 export const handler: APIGatewayProxyHandler = async function (event: APIGatewayProxyEvent) {
     console.log('request:', JSON.stringify(event, undefined, 2));
     const user = event.headers['Authorization'];
@@ -24,6 +74,7 @@ export const handler: APIGatewayProxyHandler = async function (event: APIGateway
         const templateId: string = uuid();
 
         // generate & encrypt API key
+        const encryptedApiKey: string = await generateEncryptedApiKey();
 
         // store metadata to DynamoDB
 
