6c52b39483e9c1d0cd21d7a8014cae72aa999fe4
Amy <ayung.024@gmail.com>
2021-02-18 20:24:52 -0800

    move method to templateService

--- frontend/src/components/templateComponent/templateComponent.tsx
+++ frontend/src/components/templateComponent/templateComponent.tsx
@@ -6,9 +6,6 @@ import { ViewTemplateModalComponent } from '../viewTemplateModalComponent/viewTe
 import { ToastComponentProperties, ToastInterfaces } from '../../models/toastInterfaces';
 import { ToastComponent } from '../toastComponent/toastComponent';
 import './templateComponent.css';
-import JSZip from 'jszip';
-import FileSaver from 'file-saver';
-const mammoth = require('mammoth');
 
 export class TemplateComponent extends React.Component<any, ToastComponentProperties> {
     private _service: TemplateService;
@@ -26,44 +23,6 @@ export class TemplateComponent extends React.Component<any, ToastComponentProper
     // componentDidMount(): void {
     //     this._service.getTemplates();
     // }
-
-    private convertDocxFile(input: React.ChangeEvent<HTMLInputElement>): void {
-        const files = input.target.files || [];
-        if (!files.length) return;
-        const file = files[0];
-
-        const reader = new FileReader();
-        reader.onloadend = function () {
-            const arrayBuffer = reader.result;
-            mammoth.convertToHtml({ arrayBuffer: arrayBuffer }).then(function (resultObj: any) {
-                const result1 = document.querySelector('#result1');
-                if (result1) {
-                    result1.innerHTML = resultObj.value;
-                }
-                const html = resultObj.value;
-                const regExp = /"[^"]+"/g;
-                const allImages = html.match(regExp);
-                const images = [];
-                for (const image of allImages) {
-                    if (image.includes('data:image')) {
-                        const imageData = image.slice(image.indexOf(',') + 1, image.length);
-                        const imageType = image.slice(image.indexOf('/') + 1, image.indexOf(';'));
-                        images.push([imageType, imageData]);
-                    }
-                }
-                const zip = new JSZip();
-                let count = 0;
-                for (const img of images) {
-                    zip.file('images/image' + count + '.' + img[0], img[1], { base64: true });
-                    count++;
-                }
-                zip.generateAsync({ type: 'blob' }).then(function (blob: any) {
-                    FileSaver.saveAs(blob, 'images.zip');
-                });
-            });
-        };
-        reader.readAsArrayBuffer(file);
-    }
 
     private _addToast(toast: ToastInterfaces): void {
         if (!this._toastMessages.some(messages => messages.id === toast.id)) {
@@ -87,7 +46,6 @@ export class TemplateComponent extends React.Component<any, ToastComponentProper
                 </div>
                 <div className='upload-container'>
                     <h4 className='upload-desc'>Please choose a template file to upload. Accepted file format: .docx</h4>
-                    <input type='file' onChange={this.convertDocxFile.bind(this)} />
                 </div>
                 <TemplateGridComponent addToast={this._addToast.bind(this)} />
                 <ViewTemplateModalComponent addToast={this._addToast.bind(this)} />
--- frontend/src/services/templateService.ts
+++ frontend/src/services/templateService.ts
@@ -1,4 +1,7 @@
 import { RequestService } from './requestService';
+import JSZip from 'jszip';
+import FileSaver from 'file-saver';
+const mammoth = require('mammoth');
 
 export class TemplateService {
     private _requestService: RequestService;
@@ -9,5 +12,40 @@ export class TemplateService {
 
     public getTemplates(): Promise<any> {
         return this._requestService.GET('/helloWorld');
+    }
+
+    public parseDocx(docx: File): [images: string[], html: string] {
+        let html = '';
+        const images: any[] = [];
+        const file = docx;
+
+        const reader = new FileReader();
+        reader.onloadend = function () {
+            const arrayBuffer = reader.result;
+            mammoth.convertToHtml({ arrayBuffer: arrayBuffer }).then(function (resultObj: any) {
+                const parsed = resultObj.value;
+                html = resultObj.value;
+                const regExp = /"[^"]+"/g;
+                const allImages = parsed.match(regExp);
+                for (const image of allImages) {
+                    if (image.includes('data:image')) {
+                        const imageData = image.slice(image.indexOf(',') + 1, image.length);
+                        const imageType = image.slice(image.indexOf('/') + 1, image.indexOf(';'));
+                        images.push([imageType, imageData]);
+                    }
+                }
+                const zip = new JSZip();
+                let count = 0;
+                for (const img of images) {
+                    zip.file('images/image' + count + '.' + img[0], img[1], { base64: true });
+                    count++;
+                }
+                zip.generateAsync({ type: 'blob' }).then(function (blob: any) {
+                    FileSaver.saveAs(blob, 'images.zip');
+                });
+            });
+        };
+        reader.readAsArrayBuffer(file);
+        return [images, html];
     }
 }
