5c6bdc850883bdc880d5d8591bc9aeb132903564
nappyu <tojinakabayashi@gmail.com>
2021-02-19 16:29:34 -0800

    added error messages and changed how to access database

--- backend/src/lambda/listTemplates/index.ts
+++ backend/src/lambda/listTemplates/index.ts
@@ -1,14 +1,31 @@
 import { Handler } from 'aws-lambda';
-const AWS = require('aws-sdk');
-// CHECK THE SERVER 
-AWS.config.update({region: `us-east-2`});
-const db = new AWS.DynamoDB.DocumentClient({region: 'us-east-2'});
+import * as db from "../../database/dbOperations";
 
+const AWS = require('aws-sdk');
+// check server region
+// AWS.config.update({region: `us-east-2`});
+// const db = new AWS.DynamoDB.DocumentClient({region: 'us-east-2'});
+const headers = {
+    'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+    'Content-Type': 'application/json',
+}
 export const handler: Handler = async function (event) {
     // TODO: #10
     // stub
     console.log('request:', JSON.stringify(event, undefined, 2));
     const user = event.headers['Authorization'];
+    console.log(user);
+    if (!event.body) {
+        return {
+            headers,
+            statusCode: 400,
+            body: JSON.stringify({
+                "message": "Invalid request format",
+                "code": ""
+            })
+        }
+    }
 
     // get items from start to start + limit
     const params = {
@@ -17,18 +34,31 @@ export const handler: Handler = async function (event) {
         Limit: event['limit']
     };
 
-    return db.query({
-        TableName: 'metadataTable',
-        KeyConditionExpression: '#index BETWEEN :indexLow AND :indexHigh',
-        ExpressionAttributeNames: {
-            '#id': 'id',
-            '#index': 'index',
-        },
-        ExpressionAttributeValues: {
-            ':indexLow': params.Start,
-            ':indexHigh': params.Limit,
-        },
-    }).promise();
+    const {status, metadataList} = await db.ListMetadataByDate(params.Start, params.Limit);
+    if (!status.succeeded || !metadataList) {
+        return {
+            headers,
+            statusCode: 500,
+            body: JSON.stringify({
+                "message": `Failed get templates: ${status.info}`,
+                "code": ""
+            })
+        }
+    }
+
+    // maybe Tony can use this query
+    // return db.query({
+    //     TableName: 'metadataTable',
+    //     KeyConditionExpression: '#index BETWEEN :indexLow AND :indexHigh',
+    //     ExpressionAttributeNames: {
+    //         '#id': 'id',
+    //         '#index': 'index',
+    //     },
+    //     ExpressionAttributeValues: {
+    //         ':indexLow': params.Start,
+    //         ':indexHigh': params.Limit,
+    //     },
+    // }).promise();
 
 
     return {
@@ -39,9 +69,7 @@ export const handler: Handler = async function (event) {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            eventPath: event.path,
-            test: 'listTemplates',
-            user: user,
+            metadataList
         }),
     };
 };
