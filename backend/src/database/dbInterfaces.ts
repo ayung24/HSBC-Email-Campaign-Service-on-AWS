0e01a77ec81b3f88d8308ab57e0c7c7689f0fe07
philmak <31200843+philmak@users.noreply.github.com>
2021-02-26 23:27:57 -0800

    Refactor db (#61)
    * Refactor db
    * Update gitignore
    * Remove ./idea folders
    * Add spinner to grid
    * Refresh grid after upload

---
+++ backend/src/database/dbInterfaces.ts
@@ -0,0 +1,19 @@
+// a Entry is either deleted or not
+export enum EntryStatus {
+    IN_SERVICE = 'In Service',
+    DELETED = 'Deleted',
+}
+
+// a base entry, used for list
+export interface ITemplateBase {
+    readonly templateId: string;
+    readonly timeCreated: number;
+    readonly templateStatus: EntryStatus;
+    readonly templateName: string;
+}
+
+// a full entry, used for getting details
+export interface ITemplateFullEntry extends ITemplateBase {
+    readonly apiKey: string;
+    readonly fieldNames: string[];
+}
