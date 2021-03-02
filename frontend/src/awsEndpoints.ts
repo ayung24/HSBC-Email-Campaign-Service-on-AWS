507c14ccc09834c7f9b5c88ef4a39e350ebad227
shizuko <aka-shizuko@hotmail.com>
2021-03-02 12:51:42 -0800

    Resolve api endpoint (round 2)

--- frontend/src/awsEndpoints.ts
+++ frontend/src/awsEndpoints.ts
@@ -1,7 +1,7 @@
 import { config } from './config';
 
 let region, deploymentID;
-if (process.env.NODE_ENV && process.env.NODE_ENV == 'production') {
+if (process.env.REACT_APP_BUILD_ENV && process.env.REACT_APP_BUILD_ENV == 'prod') {
     region = config.api.REGION_PROD;
     deploymentID = config.api.DEPLOYMENT_ID_PROD;
 } else {
