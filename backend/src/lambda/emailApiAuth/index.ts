c4f99e6667822cf0703ceefd0feba53ec86a24e1
shizuko <aka-shizuko@hotmail.com>
2021-03-20 16:29:07 -0700

    Add preflight env validation to auth handler

--- backend/src/lambda/emailApiAuth/index.ts
+++ backend/src/lambda/emailApiAuth/index.ts
@@ -3,21 +3,34 @@ import * as db from '../../database/dbOperations';
 import { ITemplateFullEntry } from '../../database/dbInterfaces';
 import { IEmailAPIAuthReqBody } from '../lambdaInterfaces';
 import { AWSError, KMS} from 'aws-sdk';
-import { config } from '../../../../frontend/src/config';
+import * as Logger from '../../../logger';
 
 const KMS_REGION = process.env.KMS_REGION;
 const KMS_ACCOUNT_ID = process.env.KMS_ACCOUNT_ID;
 const KMS_KEY_ID = process.env.KMS_KEY_ID;
 const METADATA_TABLE_NAME = process.env.METADATA_TABLE_NAME;
 
-const KMS_ACCESS_KEY = process.env.KMS_ACCESS_KEY;
-const KMS_SECRET_KEY = process.env.KMS_SECRET_KEY;
-
 const kms: KMS = new KMS({
-    region: config.kms.REGION
+    region: KMS_REGION,
 });
 
+/**
+ * Validates lambda's runtime env variables
+ */
+ const validateEnv = function (): boolean {
+    return !!METADATA_TABLE_NAME && !!KMS_KEY_ID && !!KMS_REGION && !!KMS_ACCOUNT_ID;
+};
+
 export const handler: APIGatewayRequestAuthorizerHandler = function (event, context, callback) {
+    Logger.info({
+        message: 'Received POST /email authorization request',
+        additionalInfo: event
+    });
+
+    if (!validateEnv()) {
+        callback("Internal server error")
+    }
+
     const headers = event.headers;
     if (!headers.TemplateId || !headers.APIKey) {
         callback("Unauthorized");
@@ -27,25 +40,38 @@ export const handler: APIGatewayRequestAuthorizerHandler = function (event, cont
         templateId: headers.TemplateId,
         apiKey: headers.APIKey
     }
-    console.log("Authorizing context: ", JSON.stringify(authContext));
+    Logger.info({
+        message: "Authorizing context", 
+        additionalInfo: authContext
+    });
 
     // Query DynamoDB to retrieve template's metadata and decrypt DB-stored API key
     db.GetTemplateById(authContext.templateId).then((template: ITemplateFullEntry) => {
-        console.log("Retrieved template: ", JSON.stringify(template))
+        Logger.info({
+            message: "Retrieved template",
+            additionalInfo: template,
+        })
         const decryptParam = {
             KeyId: `arn:aws:kms:${KMS_REGION}:${KMS_ACCOUNT_ID}:key/${KMS_KEY_ID}`,
             CiphertextBlob: Buffer.from(template.apiKey, 'base64'),
         };
         kms.decrypt(decryptParam, (err: AWSError, data: KMS.Types.DecryptResponse) => {
             if (err || !data.Plaintext) {
+                Logger.err(err);
                 callback("Unauthorized");
             } else if (data.Plaintext.toString() === authContext.apiKey) {
+                Logger.info({message: "Authorization success", additionalInfo: {templateId: template.templateId}});
                 callback(null, generatePolicy(event.requestContext.identity.userAgent, 'Allow', event.methodArn));
             } else {
+                Logger.info({
+                    message: "Authorization failure",
+                    additionalInfo: {templateId: template.templateId}
+                })
                 callback(null, generatePolicy(event.requestContext.identity.userAgent, 'Deny', event.methodArn));
             }
         });
     }).catch((error) => {
+        Logger.err(error);
         callback("Unauthorized");
     });
 };
