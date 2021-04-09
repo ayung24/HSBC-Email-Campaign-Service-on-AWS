8bd3e25cf7ff8789b990818b04911cd24d9e6f08
Clarissa Audrey <clarissalukmanto@gmail.com>
2021-04-08 17:16:03 -0700

    Add search bar frontend

--- frontend/src/components/viewTemplateModalComponent/viewTemplateModalComponent.tsx
+++ frontend/src/components/viewTemplateModalComponent/viewTemplateModalComponent.tsx
@@ -482,7 +482,7 @@ export class ViewTemplateModalComponent extends React.Component<ViewTemplateModa
                                                         variant='outline-secondary'
                                                         onClick={event => this._copyText(this.state.apiKey, event)}
                                                     >
-                                                        <Image src={copyImage} alt='copy icon' fluid />
+                                                        <Image src={copyImage} alt='copy icon' />
                                                     </Button>
                                                 </InputGroup.Append>
                                             </InputGroup>
